61acea035f914fa952ad7708a6a61f98652a7776
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2015-08-13 13:56:45 +0200

    Fail on failing tests

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ class PyTest(TestCommand):
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
-        pytest.main(self.test_args)
+        errno = pytest.main(self.test_args)
+        raise SystemExit(errno)
 
 
 readme = open('README.rst').read()
