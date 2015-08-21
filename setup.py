ca7f0c03d747d4a97edc47b17b82c3c6ae336196
Heiko Heiko <heiko@heiko.org>
2015-08-21 08:44:06 +0200

    version 0.5.0 using gevent 1.1b3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ install_requires = [install_requires_replacements.get(r, r) for r in install_req
 
 setup(
     name='devp2p',
-    version='0.4.5',
+    version='0.5.0',
     description='Python implementation of the Ethereum P2P stack',
     long_description=readme + '\n\n' + history,
     author='HeikoHeiko',
