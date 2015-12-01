7d62858efe1a296d8ce496015bff51f3ca7f4e22
Paweł Bylica <chfast@gmail.com>
2015-12-01 09:47:34 +0100

    Fix logging patching. Don't expect `msg` argument to be a string.
    The logging patching affects whole application and all used packages that use logging. In some use cases `msg` is not a string. Stringify it first before appending kwargs.

--- devp2p/slogging.py
+++ devp2p/slogging.py
@@ -10,8 +10,9 @@ except ImportError:
     _log_orig = logging.Logger._log
 
     def _kargs_log(self, level, msg, args, exc_info=None, extra=None, **kargs):
-        msg += ' ' + ' '.join('%s=%r' % (k, v) for k, v in kargs.items())
-        _log_orig(self, level, msg, args, exc_info, extra)
+        kwmsg = ''.join(' %s=%s' % (k, str(v)) for k, v in kargs.items())
+        _log_orig(self, level, str(msg) + kwmsg, args, exc_info, extra)
+
     logging.Logger._log = _kargs_log
     get_logger = logging.getLogger
 
