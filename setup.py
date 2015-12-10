0f9bdbcd323e34bd7bc452092730873ecfb932f1
Ulrich Petri <ulo@ulo.pe>
2015-12-10 17:34:25 +0100

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ test_requirements = [
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '0.6.0'
+version = '0.6.1'
 
 setup(
     name='devp2p',
